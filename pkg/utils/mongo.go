76db71e01a4dee7e74a5242d8a3a6560de3012dc
Kami0rn <tew965@gmail.com>
2024-07-26 05:07:02 +0700

    "Added new structs and functions to models and utils packages"

--- pkg/utils/mongo.go
+++ pkg/utils/mongo.go
@@ -1,1 +1,11 @@
 package utils
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ConvertToObject(id string) primitive.ObjectID {
+	objectId , _ := primitive.ObjectIDFromHex(id)
+	return objectId
+}
+
